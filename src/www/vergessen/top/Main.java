a5a24c376f9c439afb51b87d273c4f148d9c7f8b
root <1258252786@qq.com>
2020-01-14 16:43:24 +0800

    加入配置文件。配置更灵活

--- src/www/vergessen/top/Main.java
+++ src/www/vergessen/top/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
 
+        int initTankCount = Integer.parseInt((String) PropertyMgr.get("initTankCount"));
         //初始化敌方坦克
-        for(int i = 0; i < 10 ; i++){
-            tf.badTank.add(new Tank(60 + i * 70
+        for(int i = 0; i < initTankCount ; i++){
+            tf.badTank.add(new Tank(30 + i * 70
                     ,100,Dir.DOWN,Group.BAD,tf));
         }
 
